3c74f01a8b85b0eb912217f70869b135a0e26b27
rajgaurav1989 <rajgaurav1989.bits10@gmail.com>
2015-10-02 21:42:10 +0530

    program commit

---
+++ prog.c
@@ -0,0 +1,23 @@
+void abc(int i)
+{	
+	int a = 0 ;
+	for (i = 0 ; i < 10 ; i++)
+		a++ ;
+	
+}
+
+int main()
+{
+	int i , j ,k = 0 ,a = 0 ,l , m;
+	for (i = 0 ; i < 10 ; i++)
+		for (j = 0 ; j < 10 ; j++)
+			k++ ;
+
+	for (i = 0 ; i < 10 ; i++) 
+		k++ ;
+	abc(k) ;
+	for (i = 0 ; i < 10 ; i++)
+		for(j = 0 ; j < 10 ; j++)
+			a++;
+	return 0 ;
+}
